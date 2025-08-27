273662cc3cf62cfed2fc1d26bbd5dae2cc6dfb8d
JangIkhwan <bluensky0213@gmail.com>
2025-08-27 17:48:07 +0900

    [Fix] #6 리포지토리 메서드 수정

--- src/main/java/org/example/backend/domain/users/repository/UserRepository.java
+++ src/main/java/org/example/backend/domain/users/repository/UserRepository.java
@@ -2,9 +2,11 @@ package org.example.backend.domain.users.repository;
 
 import org.example.backend.domain.users.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
 }
