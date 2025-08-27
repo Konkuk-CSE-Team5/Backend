5e574036bb2356a5a4f28bff2d790c760bb3d6f2
JangIkhwan <bluensky0213@gmail.com>
2025-08-27 12:51:11 +0900

    [Feat] #6 커스텀 UserDetailsService 구현

---
+++ src/main/java/org/example/backend/global/auth/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package org.example.backend.global.auth.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.backend.domain.users.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(user ->{
+                    log.info("[loadUserByUsername] User was found. user = {}",  user);
+                    return new CustomUserDetails(user);
+                })
+                .orElseThrow(() -> {
+                    log.info("[loadUserByUsername] User with email {} cannot found!!", username );
+                    throw new UsernameNotFoundException("User with email" + username + " cannot found!!");
+                });
+    }
+}
