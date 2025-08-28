d3770a4f4a9d2a2c780d7f0ce45998bc2223e0b2
JangIkhwan <bluensky0213@gmail.com>
2025-08-29 02:34:11 +0900

    [Feat] #37 어르신 등록 화면 조회 API 명세

--- src/main/java/org/example/backend/domain/organization/controller/OrganizationController.java
+++ src/main/java/org/example/backend/domain/organization/controller/OrganizationController.java
@@ -77,8 +77,14 @@ public class OrganizationController {
         return new BaseResponse<>(organizationMainService.getMain(userId));
     }
 
+    @Operation(
+            summary = "기관 어르신 등록 화면 조회",
+            description = "어르신 등록 화면을 조회하는 API"
+    )
+    @CustomExceptionDescription(MAIN)
+    @Parameter(in = ParameterIn.HEADER, required = true, name = "Authorization", description = "API 엑세스 토큰", example = "Bearer asdf1234")
     @GetMapping("/me/seniors")
-    public BaseResponse<GetOrgSeniorsResponse> getOrgSeniors(@LoginUserId Long orgUserId){
+    public BaseResponse<GetOrgSeniorsResponse> getOrgSeniors(@Parameter(hidden = true) @LoginUserId Long orgUserId){
         return new BaseResponse(getOrgSeniorsService.getSeniors(orgUserId));
     }
 }
