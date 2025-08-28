ad3b978b573996e50c542586b4c68272f08a1861
JangIkhwan <bluensky0213@gmail.com>
2025-08-28 15:24:59 +0900

    [Fix] #12 봉사 레코드 상태 상수 수정

--- src/main/java/org/example/backend/domain/record/model/VolunteerRecordStatus.java
+++ src/main/java/org/example/backend/domain/record/model/VolunteerRecordStatus.java
@@ -3,6 +3,5 @@ package org.example.backend.domain.record.model;
 public enum VolunteerRecordStatus {
     COMPLETE,    // 완료
     ABSENT, // 부재중
-    NOT_CONDUCTED, // 미실시
-    PENDING// 시작 전
+    NOT_CONDUCTED // 미실시
 }
