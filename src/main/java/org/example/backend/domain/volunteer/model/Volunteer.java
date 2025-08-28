a23146cfa0a86dbff199146cba838abc23aa7d19
JangIkhwan <bluensky0213@gmail.com>
2025-08-28 11:11:20 +0900

    [Feat] #10 봉사자 엔티티와 레포지토리 추가

---
+++ src/main/java/org/example/backend/domain/volunteer/model/Volunteer.java
@@ -0,0 +1,35 @@
+package org.example.backend.domain.volunteer.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.backend.domain.users.model.User;
+import org.example.backend.global.common.model.BaseEntity;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Volunteer extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate birthday;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String phone;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
