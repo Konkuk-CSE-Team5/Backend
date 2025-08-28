8e053fecbc5f406f825575bdb1abd9e08bbc912f
JangIkhwan <bluensky0213@gmail.com>
2025-08-29 00:14:04 +0900

    [Feat] #22 API 명세 추가

--- src/main/java/org/example/backend/domain/senior/controller/SeniorController.java
+++ src/main/java/org/example/backend/domain/senior/controller/SeniorController.java
@@ -1,16 +1,26 @@
 package org.example.backend.domain.senior.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.backend.domain.senior.dto.GetSeniorMatchingRecords;
 import org.example.backend.domain.senior.service.OrganizationSeniorService;
 import org.example.backend.global.auth.annotation.LoginUserId;
 import org.example.backend.global.common.response.BaseResponse;
+import org.example.backend.global.swagger.CustomExceptionDescription;
+import org.example.backend.global.swagger.SwaggerResponseDescription;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(
+        name = "Senior",
+        description = "어르신 관련 API"
+)
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("seniors")
@@ -19,6 +29,13 @@ public class SeniorController {
     private final OrganizationSeniorService seniorService;
 
     // 기관 어르신 활동 기록 조회
+
+    @Operation(
+            summary = "기관 어르신 현황 조회",
+            description = "기관의 어르신 현황을 조회하는 API"
+    )
+    @CustomExceptionDescription(SwaggerResponseDescription.MAIN)
+    @Parameter(in = ParameterIn.HEADER, required = true, name = "Authorization", description = "API 엑세스 토큰", example = "Bearer asdf1234")
     @GetMapping("/{seniorId}/records")
     public BaseResponse<GetSeniorMatchingRecords> getCurrentMatchingRecords(@LoginUserId Long userId, @PathVariable Long seniorId){
         return new BaseResponse<>(seniorService.getCurrentMatchingRecords(userId, seniorId));
