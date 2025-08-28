2ed231cbfc4bb8273726d0d63262d560596db5ee
JangIkhwan <bluensky0213@gmail.com>
2025-08-28 23:52:56 +0900

    [Feat] #22 기관의 어르신 활동 조회 API 수정

--- src/main/java/org/example/backend/domain/matching/repository/MatchingRepository.java
+++ src/main/java/org/example/backend/domain/matching/repository/MatchingRepository.java
@@ -6,12 +6,14 @@ import org.example.backend.domain.senior.model.Senior;
 import org.example.backend.domain.volunteer.model.Volunteer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface MatchingRepository extends JpaRepository<Matching, Long> {
+    Optional<Matching> findBySeniorIdAndMatchingStatus(Long seniorId, MatchingStatus matchingStatus);
+
     Optional<Matching> findByVolunteerIdAndSeniorIdAndMatchingStatus(Long volunteerId, Long seniorId, MatchingStatus status);
+
     List<Matching> findAllByVolunteer(Volunteer volunteer);
 
     List<Matching> findAllBySeniorAndMatchingStatus(Senior senior, MatchingStatus matchingStatus);
--- src/main/java/org/example/backend/domain/senior/controller/SeniorController.java
+++ src/main/java/org/example/backend/domain/senior/controller/SeniorController.java
@@ -3,7 +3,7 @@ package org.example.backend.domain.senior.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.backend.domain.senior.dto.GetSeniorMatchingRecords;
-import org.example.backend.domain.senior.service.SeniorService;
+import org.example.backend.domain.senior.service.OrganizationSeniorService;
 import org.example.backend.global.auth.annotation.LoginUserId;
 import org.example.backend.global.common.response.BaseResponse;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("seniors")
 @RestController
 public class SeniorController {
-    private final SeniorService seniorService;
+    private final OrganizationSeniorService seniorService;
+
+    // 기관 어르신 활동 기록 조회
     @GetMapping("/{seniorId}/records")
     public BaseResponse<GetSeniorMatchingRecords> getCurrentMatchingRecords(@LoginUserId Long userId, @PathVariable Long seniorId){
         return new BaseResponse<>(seniorService.getCurrentMatchingRecords(userId, seniorId));
